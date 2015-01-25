b9223ce78ac9d91f2b7e3583ba62ce1b18aa0db2
julienbrunuk <hello@julienbrun.co.uk>
2015-01-25 12:31:44 +0000

    mapping

--- app.ui.js
+++ app.ui.js
@@ -1,14 +1,40 @@
-function editorUI(){
-  this.new = function(){
-    // create new artboard
-  };
-  this.editPx = function(loc_x, loc_y, color){
-    // update a pixel
-  };
-  this.getPx = function(loc_x, loc_y){
-    // get pixel color
-  };
-  this.new(arguments); // initialise
-};
+$(document).ready(function() {
+    function editorUI(El) {
+        this.size = 16 * 16; // always a square!
+        this.coordinates = function(pos) {
+            var LineLength = Math.sqrt(this.size);
+            var x = pos % LineLength;
+            var y = Math.floor(pos / LineLength);
 
-var thisEditor = new editorUI();
+            return ({
+                x: x,
+                y: y
+            });
+        }
+        this.addPx = function(pos) {
+            console.log(El);
+            $(El).append($('<div/>', {
+                class: 'pixel',
+                data: this.coordinates(pos)
+            }));
+        };
+        this.new = function(El) {
+            var PixelsTotal = 0;
+            var PixelsTodo = 16 * 16;
+            while (PixelsTotal < PixelsTodo) {
+                this.addPx(PixelsTotal); // where pos = PixelsTotal
+                PixelsTotal++;
+            }
+        };
+        this.editPx = function(loc_x, loc_y, color) {
+            // update a pixel
+        };
+        this.getPx = function(loc_x, loc_y) {
+            // get pixel color
+        };
+        this.element = this.new(El); // initialise
+    };
+
+
+    var thisEditor = new editorUI($('.editor'));
+});
